c9ba30d1d431558feea9a0eaee429d349d2d0ea8
Jorge Ferreyra <jorxfer@gmail.com>
2025-06-01 16:24:53 -0300

    Adding audio mini-game

---
+++ src/scripts/audio.js
@@ -0,0 +1,17 @@
+export function validatePassword() { 
+  const finalAnswer = 'momo quien te corto el pelo para no ir?'
+  const inputAnswer = document.getElementById('respuesta-audio')
+  const inputValue = inputAnswer.value.toLowerCase().trim()
+
+
+  const inputInfo = document.getElementById('info')
+
+  if (inputValue === finalAnswer) {
+    inputInfo.value = 'Acceso concedido';
+  } else {
+    alert("contraseña incorrecta")
+    inputInfo.value = '';
+  }
+}
+
+window.validatePassword = validatePassword
